3d56c1761d32361ad1d55178ceccfa96bc08aef6
petermclagan <peter.mclagan94@gmail.com>
2020-09-15 21:10:00 +0100

    Updated version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as r:
 
 setup(
 	name='betfair-historical',
-	version='1.0.0',
+	version='1.1.0',
 	description='www.api-football.com tooling',
 	author='Peter McLagan',
 	author_email='peter.mclagan94@gmail.com',
