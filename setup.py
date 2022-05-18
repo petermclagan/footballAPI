d2f9d35dbff2660bba3a52e94d8d233f69157784
Peter McLagan <peter.mclagan94@gmail.com>
2022-05-18 18:23:21 +0100

    PEP8 formatting of setup

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 with open('requirements.txt') as r:
-	requirements = r.readlines()
+    requirements = r.readlines()
 
 setup(
-	name='APIFootball',
-	version='1.1.0',
-	description='www.api-football.com tooling',
-	author='Peter McLagan',
-	author_email='peter.mclagan94@gmail.com',
-	url='https://github.com/petermclagan/footballAPI',
-	packages=find_packages(),
-	include_package_data=True,
-	install_requires=requirements,
-	license='MIT',
-	zip_safe=False
-	)
+    name='APIFootball',
+    version='1.1.0',
+    description='www.api-football.com tooling',
+    author='Peter McLagan',
+    author_email='peter.mclagan94@gmail.com',
+    url='https://github.com/petermclagan/footballAPI',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    license='MIT',
+    zip_safe=False
+)
